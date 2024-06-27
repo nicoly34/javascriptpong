14613704c9da92ff8d49716671ab026b4c36a903
nicoly34 <nicoly.laureano@escola.pr.gov.br>
2024-06-27 17:41:33 -0300

    cria bolinha 27/06/24

--- pingpong.js
+++ pingpong.js
@@ -1,3 +1,7 @@
+//aqui eu vou criar a bolinha
+let xBolinha = 300;
+let yBolinha = 200;
+let tambolinha = 25;
 function setup(){
     //aqui vou criar minha "mesa"
     createCanvas(600,400);
@@ -7,5 +11,12 @@ function draw(){
     //aqui vou por a cor da "mesa"
     //A cor da mesa é em rgb
     background(128,128,0); 
+    //chamando a função
+    criaBolinha (xBolinha,yBolinha,tambolinha)
 }
 
+
+//função bolinha
+function criaBolinha(xBolinha,yBolinha,tambolinha){
+    circle (xBolinha,yBolinha,tambolinha);
+}
