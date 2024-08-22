20fe86508379cf1a83248a8d6d3cd6aa5b1a2880
nicoly34 <nicoly.laureano@escola.pr.gov.br>
2024-08-22 12:11:41 -0300

    Update pingpong.js

--- pingpong.js
+++ pingpong.js
@@ -1,22 +1,79 @@
 //aqui eu vou criar a bolinha
 let xBolinha = 300;
 let yBolinha = 200;
-let tambolinha = 25;
+let tamBolinha = 25;
+let raioBolinha = tamBolinha/2;
+
+//config velocidade bolinha
+let xvelocidadeBolinha = 6;
+let yvelocidadeBolinha = 6;
+
+let xRaquete = 5;
+let yRaquete = 150;
+let larguraRaquete = 10;
+let alturaRaquete = 90;
+
 function setup(){
     //aqui vou criar minha "mesa"
     createCanvas(600,400);
 }
 
 function draw(){
+    //função responsável pelo "desenho e animação da mesa"
     //aqui vou por a cor da "mesa"
     //A cor da mesa é em rgb
     background(128,128,0); 
-    //chamando a função
-    criaBolinha (xBolinha,yBolinha,tambolinha)
+    //chamando a função cria bolinha para criar a bolinha
+    criaBolinha(xBolinha, yBolinha, tamBolinha);
+    //chamando a função mov bolinha
+    moveBolinha();
+    //chamando a borda       
+    Borda();
+    criaRaquete(xRaquete, yRaquete, larguraRaquete, alturaRaquete);
+    movimentaRaquete();
+    colideRaquete();
 }
 
-
 //função bolinha
-function criaBolinha(xBolinha,yBolinha,tambolinha){
-    circle (xBolinha,yBolinha,tambolinha);
+function criaBolinha(xBolinha, yBolinha,tamBolinha){
+    fill("red")
+    circle (xBolinha,yBolinha,tamBolinha);
+}
+
+//função move bolinha
+function moveBolinha(){
+    xBolinha = xvelocidadeBolinha + xBolinha;
+    yBolinha = yvelocidadeBolinha + yBolinha;
+}
+
+function Borda(){
+    if (xBolinha > width || xBolinha < 0){
+        xvelocidadeBolinha *= -1;
+    }
+    if (yBolinha > height || yBolinha < 0){
+        yvelocidadeBolinha *= -1;
+    }
+}
+
+//função responsavel por criar o retangulo que representa a raquete
+function criaRaquete(xRaquete, yRaquete, larguraRaquete, alturaRaquete){
+    fill("blue");
+    rect(xRaquete, yRaquete, larguraRaquete, alturaRaquete);
+}
+
+//função responsavel por movimentar a raquete
+function movimentaRaquete(){
+if(keyIsDown(UP_ARROW)){
+    yRaquete -= 10;
+}
+if(keyIsDown(DOWN_ARROW)){
+    yRaquete += 10;
+}
+}
+
+function colideRaquete(){
+    if(xBolinha - raioBolinha < xRaquete + larguraRaquete && yBolinha - raioBolinha < yRaquete + alturaRaquete && yBolinha + raioBolinha > yRaquete){
+
+        xvelocidadeBolinha *=-1;
+    }
 }
